09312e62378344f4fe7e93ddd1fcc658eeb278bb
Lotosney <sebastiandwojcik@gmail.com>
2021-02-24 14:03:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import About from './components/About'
+function App() {
+  return (
+    <div className="container">
+      <Header title="Hello"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const header = (props) => {
+    return (
+    <header>
+        <h1>{props.title}</h1>
+        </header>
+    )
+}
+
+export default header
